f8fad4a0f7a2eabc9f7dae83e808326c8ad07394
yanchao <yanchao727@gmail.com>
2012-12-29 16:09:23 +0800

    add activity.html

--- activity/urls.py
+++ activity/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('fuxing.activity.views',
     url(r'^info/$','info',name='info'),
+    url(r'^$','activity',name='activity'),
 	)
 
--- activity/views.py
+++ activity/views.py
@@ -3,6 +3,9 @@ from django.template import RequestContext
 def info(request):
     return render_to_response('activity_info.html', context_instance=RequestContext(request))
 
+def activity(request):
+    return render_to_response('activity.html', context_instance=RequestContext(request))
+
 '''
 import logging
 from intemass.classroom.models import Classroom
