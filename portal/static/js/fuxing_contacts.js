c00b4d1d7eea21bdfb51ec826b9a64325b3f7dea
yanchao <yanchao727@gmail.com>
2013-01-30 09:20:48 +0800

    add essential js

---
+++ portal/static/js/fuxing_contacts.js
@@ -0,0 +1,12 @@
+/*
+ * yanchao727@gmail.com
+ * 30/01/2013
+ *
+ */
+
+;(function($, undef) {
+
+    $(function(){
+                $('#menu_02').addClass('active');
+	});
+})(jQuery);
---
+++ portal/static/js/fuxing_home.js
@@ -0,0 +1,12 @@
+/*
+ * yanchao727@gmail.com
+ * 30/01/2013
+ *
+ */
+
+;(function($, undef) {
+
+    $(function(){
+                $('#menu_00').addClass('active');
+	});
+})(jQuery);
