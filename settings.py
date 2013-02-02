6611d49665185c60605413932d7d0b6f5d40bc9e
yanchao <your_email@youremail.com>
2013-02-03 07:56:35 +0800

    modify settings.py

--- settings.py
+++ settings.py
@@ -24,9 +24,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',     # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'fuxing',               # Or path to database file if using sqlite3.
-            'USER': 'demo',               # Not used with sqlite3.
-            'PASSWORD': '1',                # Not used with sqlite3.
+            'NAME': 'peoplel6_fuxing',               # Or path to database file if using sqlite3.
+            'USER': 'peoplel6_fuxing',               # Not used with sqlite3.
+            'PASSWORD': 'jessie61-PEOPLELINKED',                # Not used with sqlite3.
             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                               # Set to empty string for default. Not used with sqlite3
         }
